f4404c20fb4451a26405494562062ec0768e3747
nieri <murilo.nieri@hotmail.com>
2024-07-16 15:18:22 -0300

    first commit

---
+++ src/main/java/tech/munieri/spring/security/twitter_simplificado/TwitterSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package tech.munieri.spring.security.twitter_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/munieri/spring/security/twitter_simplificado/TwitterSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.munieri.spring.security.twitter_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
